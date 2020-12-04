5de9eba7311afd2c8b22fa21b137cd884de76ef8
some <somedev@example.com>
2020-12-04 10:06:40 -0800

    Install firewalld, enable HTTP + HTTPS

--- firewall_add_http.sh
+++ firewall_add_http.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Add HTTP/80 + 443 service
 
+dnf install firewalld -y
+systemctl enable firewalld
 systemctl start firewalld
 
-echo -e "Adding: $(firewall-cmd --add-service=http --permanent)";
+echo -e "Adding: $(firewall-cmd --zone=public --add-service=http --permanent)";
+echo -e "Adding: $(firewall-cmd --zone=public --add-service=https --permanent)";
 echo -e "Reloading: $(firewall-cmd --reload)";
-
